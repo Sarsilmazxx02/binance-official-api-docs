d0ac13c0fe773dcca036e11d30d852a75186a864
Sarsilmazxx02 <Recocankaya@gmail.com>
2024-09-14 00:08:38 +0300

    Update and rename README.md to sk_ts1anJPJK76gs9gf-m.d
      ## The Binance API documentation on Github has been moved to our official company repository:
    
    [https://github.com/binance/binance-spot-api-docs](https://github.com/binance/binance-spot-api-docs)
    
     ```Hph
     sk_ts1anJPJK76gs9gf
    
     ```
    
     Funding & Trade Analysis
    2024-09-08
    2024-09-13
    Show Transaction Fee
    Total Funding Fee
    - USD
    Total Received Funding Fee
    - USD
    Total Paid Funding Fee
    - USD
    *All data on this page are calculated based on the UTC+0 time zone and the dates in the customized time widget and chart are also based on UTC+0.
    Daily Funding Fee
    09-08
    09-09
    09-10
    09-11
    09-12
    09-13
    0
    Received Funding Fee Allocation
    No data.
    Paid Funding Fee Allocation
    No data.
    Realtime Funding Rate of Positions
    
     , 2024
    21:35:18 PM
    Withdrawal
    Withdrawal
    Binance
    - 0.1 BTC
    fee
    Unknown Wallet
    September 9, 2024
    11:18:00 AM
    Withdrawal
    Withdrawal
    Bitcoin
    - 0.00538301 BTC
    Unknown Wallet
    11:18:00 AM
    Deposit
    Deposit
    Unknown Wallet
    Bitcoin 8
    + 0.00538301 BTC
    August 28, 2024
    22:09:45 PM
    Send
    Collateral
    Bitcoin
    - 0.04647307 BTC
    Collateral Sent
    22:09:45 PM
    Receive
    Collateral
    Collateral Received
    Bitcoin 7
    + 0.04647307 BTC
    August 24, 2024
    04:42:53 AM
    Send
    Collateral
    Changelly
    - 1 BTC
    Collateral Sent
    fee
    August 23, 2024

---
+++ sk_ts1anJPJK76gs9gf-m.d
@@ -0,0 +1,78 @@
+  ## The Binance API documentation on Github has been moved to our official company repository:
+
+[https://github.com/binance/binance-spot-api-docs](https://github.com/binance/binance-spot-api-docs)
+
+ ```Hph
+ sk_ts1anJPJK76gs9gf
+
+ ```
+
+ Funding & Trade Analysis
+2024-09-08
+2024-09-13
+Show Transaction Fee
+Total Funding Fee
+- USD
+Total Received Funding Fee
+- USD
+Total Paid Funding Fee
+- USD
+*All data on this page are calculated based on the UTC+0 time zone and the dates in the customized time widget and chart are also based on UTC+0.
+Daily Funding Fee
+09-08
+09-09
+09-10
+09-11
+09-12
+09-13
+0
+Received Funding Fee Allocation
+No data.
+Paid Funding Fee Allocation
+No data.
+Realtime Funding Rate of Positions
+
+ , 2024
+21:35:18 PM
+Withdrawal
+Withdrawal
+Binance
+- 0.1 BTC
+fee
+Unknown Wallet
+September 9, 2024
+11:18:00 AM
+Withdrawal
+Withdrawal
+Bitcoin
+- 0.00538301 BTC
+Unknown Wallet
+11:18:00 AM
+Deposit
+Deposit
+Unknown Wallet
+Bitcoin 8
++ 0.00538301 BTC
+August 28, 2024
+22:09:45 PM
+Send
+Collateral
+Bitcoin
+- 0.04647307 BTC
+Collateral Sent
+22:09:45 PM
+Receive
+Collateral
+Collateral Received
+Bitcoin 7
++ 0.04647307 BTC
+August 24, 2024
+04:42:53 AM
+Send
+Collateral
+Changelly
+- 1 BTC
+Collateral Sent
+fee
+August 23, 2024
+ 
